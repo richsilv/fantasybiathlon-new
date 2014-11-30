e134b66935b9344b93c32486e65c76707d04a84a
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-11-30 16:42:40 +0000

    changes

--- server/methods/race_methods.js
+++ server/methods/race_methods.js
@@ -4,11 +4,11 @@
 
 Meteor.methods({
 
-	'race_methods/add_crons': function() {
+	'race_methods/add_jobs': function() {
 
 		CollectionFunctions.isAdmin(this.userId, true);
 
-		addCrons();
+		addJobs();
 
 	},
 
@@ -26,16 +26,24 @@ Meteor.methods({
 
 		startCron();
 
+	},
+
+	'race_methods/reset_cron': function() {
+
+		resetCron();
+
 	}
 
 });
 
-function addCrons() {
+function addJobs() {
 
 	var now = new Date(),
-		races = Races.find({RaceStart: {$gt: now}});
+		races = Races.find({StartTime: {$gt: now}});
 
 	races.forEach(function(race) {
+
+		console.log(race);
 
 		SyncedCron.add({
 			name: race.RaceId,
@@ -68,7 +76,7 @@ function checkEvents() {
 	events.forEach(function(event) {
 
 		_.each([1, 3, 7], function(days) {
-			var schedule = new moment(event.StartDate).subtract(days, 'd').toString();
+			var schedule = new moment(event.StartDate).subtract(days, 'd').toDate();
 
 			SyncedCron.add({
 				name: event.EventId + days.toString() + 'd',
@@ -92,12 +100,19 @@ function startCron() {
 
 }
 
+function resetCron() {
+
+	SyncedCron._reset();
+
+}
+
 // EXPOSE GLOBAL Methods
 
 RaceMethods = {
 
-	addCrons: addCrons,
+	addJobs: addJobs,
 	checkEvents: checkEvents,
-	startCron: startCron
+	startCron: startCron,
+	resetCron: resetCron
 
 };
